e9670327ef1dc9e5efff0cc3fb82b1e7b017a410
8318 <shikoh8318@gmail.com>
2018-07-06 00:04:09 +0300

    Configures App and Creates Bucketlist model

---
+++ instance/config.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+# get the root directory and add it to the system path
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# sys.path.insert(0, basedir)
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
+
+
